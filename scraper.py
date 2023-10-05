0112290da754172a39433675575a6a8b004c3dbb
Luke Waehner <luke.waehner@gmail.com>
2023-10-05 17:39:00 -0400

    Added SA, YF, MW support retreiving 5 articles per
    Cleaned up method of getting <article> divs and changed to get container
    Then find_all() of the <a> elements to return href and text
    Added a script to remove Keys of the same value (likely to be duped)
    
    To add: List/Set/Dict of Valid NYSE/F500 stock tickers to stop error
    from making a typo.
    Make input get forced to capitalize -> cap_str = input.upper()

--- scraper.py
+++ scraper.py
@@ -1,29 +1,68 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+
+
+def Scrape(symbol):
+
+    urls = {"Yahoo Finance": "https://finance.yahoo.com/quote/" + symbol + "?p=" + symbol + "&.tsrc=fin-srch",
+            "Seeking Alpha": "https://seekingalpha.com/symbol/" + symbol + "/news",
+            "Market Watch": "https://www.marketwatch.com/investing/stock/" + symbol + "?mod=search_symbol"
+            }
+    articles = {}  # dict key -> title, value -> url
+    linksYF = linksSA = 0
+    for search in urls:
+        response = requests.get(urls[search])
+
+        if response.status_code == 200:
+            html = response.text
+        else:
+            print("Failed to retrieve the page from:" + search)
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        if search == "Seeking Alpha":
+            news_articles = soup.find(
+                'div', attrs={"data-test-id": "post-list"})
+        elif search == "Yahoo Finance":
+            news_articles = soup.find(
+                'div', id="quoteNewsStream-0-Stream")
+        elif search == "Market Watch":
+            filter_source = soup.find_all(
+                'mw-tabs')
+            if len(filter_source) >= 2:
+                news_articles = filter_source[1]
+
+        link_element = news_articles.find_all('a', href=True)
+
+        for links in link_element:
+            url = links.get('href')
+            title = links.text.strip()
+            if search == "Yahoo Finance":
+                if (linksYF < 5 and len(articles) < len(urls)*5):
+                    articles[title] = "https://finance.yahoo.com/" + url
+                    linksYF += 1
+            elif search == "Seeking Alpha":
+                if (linksSA < 5 and len(articles) < len(urls)*5):
+                    if 'comments' and 'comment' not in links.get_text().lower():
+                        articles[title] = "https://seekingalpha.com/" + url
+                        linksSA += 1
+            elif search == "Market Watch":
+                if (len(articles) < len(urls)*5):
+                    articles[title] = url
+
+    unique_articles = {}
+
+    # Iterate through the original dictionary and add unique keys and values to the new dictionary
+    for key, value in articles.items():
+        if key not in unique_articles:
+            unique_articles[key] = value
+
+    for key, value in unique_articles.items():
+        print(key + ":")
+        print(value)
+        print()
+
 
 symbol = input("Enter a NYSE stock ticker: ")
-url = "https://seekingalpha.com/symbol/" + symbol + "/news"
-response = requests.get(url)
-
-if response.status_code == 200:
-    html = response.text
-else:
-    print("Failed to retrieve the page from:" + url)
-
-
-soup = BeautifulSoup(html, 'html.parser')
-
-# find and extract the articles -> from link with class jf_G (idk why class named this)
-
-news_articles = soup.find_all(
-    'article', class_='gC_gX fN_gX r_4 s_aQ s_bd kX_JD s_aE s_bH kX_JD s_aE s_bH kX_JH kX_JH gC_z5')
-
-for article in news_articles:
-    link_element = article.find('a', href=True)
-    if link_element:
-        url = link_element['href']
-        title = link_element.text.strip()
-        print("Title:", title)
-        print("URL:", "https://seekingalpha.com/" + url, "\n")
-    else:
-        print("URL not found")
+Scrape(symbol)
